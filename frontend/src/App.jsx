440a540c933244bb01642f7edc0e3798ce634b5a
NAxTIM17 <naxtim14@gmail.com>
2024-10-25 17:59:47 -0300

    feat: add icons library and add feature to change canvas size.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,123 +1,191 @@
-import './index.css'
-import { LazyBrush } from 'lazy-brush'
-import { useRef, useEffect, useMemo, useState } from 'react';
+import "./index.css";
+import { LazyBrush } from "lazy-brush";
+import { Pencil, Eraser, PenLine, Hand } from "lucide-react";
+
+import { useRef, useEffect, useMemo, useState } from "react";
 
 function App() {
   const LAZY_RADIUS = 60;
-  const BRUSH_RADIUS = 10;
+  const BRUSH_RADIUS = 5;
   const isDrawingRef = useRef(false);
+  const canvasContainerRef = useRef();
+  // const lastPosRef = useRef({ x: 0, y: 0 }); // Ref for tracking the last position
 
   const canvasRef = useRef();
 
-  
   const lazy = useMemo(() => {
     return new LazyBrush({
       enabled: true,
       radius: LAZY_RADIUS,
       initialPoint: {
         x: 0,
-        y: 0
-      }
-    })
-  },[LAZY_RADIUS]) 
-  
+        y: 0,
+      },
+    });
+  }, [LAZY_RADIUS]);
+
   const lazyFriction = useMemo(() => {
     return new LazyBrush({
       enabled: true,
       radius: LAZY_RADIUS,
       initialPoint: {
         x: 0,
-        y: 0
-      }
+        y: 0,
+      },
     });
   }, [LAZY_RADIUS]);
 
-  useEffect(()=>{
+  useEffect(() => {
     const canvas = canvasRef.current;
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    const ctx = canvas.getContext('2d');
+    const canvasContainer = canvasContainerRef.current;
 
-    const offscreenCanvas = document.createElement('canvas');
+    const { width, height } = window.getComputedStyle(canvasContainer);
+
+    canvas.width = parseInt(width.split("p")[0]);
+    canvas.height = parseInt(height.split("p")[0]);
+
+    const ctx = canvas.getContext("2d");
+    const points = [];
+
+    const offscreenCanvas = document.createElement("canvas");
     offscreenCanvas.width = canvas.width;
     offscreenCanvas.height = canvas.height;
-    const offscreenCtx = offscreenCanvas.getContext('2d');
+    const offscreenCtx = offscreenCanvas.getContext("2d");
 
     const handleMouseDown = (event) => {
+      console.log("press")
       isDrawingRef.current = true;
-      const x = event.clientX;
-      const y = event.clientY;
+      const rect = canvas.getBoundingClientRect();
+      const x = event.x - rect.left;
+      const y = event.y - rect.top;
 
-      lazyFriction.update({ x, y }, {both: true});
-      
-        offscreenCtx.beginPath()
-        offscreenCtx.moveTo(x, y);
+      offscreenCtx.beginPath();
+      offscreenCtx.strokeStyle = "#000000";
+      offscreenCtx.lineWidth = 7;
+      offscreenCtx.lineCap = "round";
+      offscreenCtx.lineTo(x, y);
+      offscreenCtx.stroke();
+    };
+    const midPointBtw = (p1, p2) => {
+      return {
+        x: p1.x + (p2.x - p1.x) / 2,
+        y: p1.y + (p2.y - p1.y) / 2,
+      };
     }
-    
     const handleMouseMove = (event) => {
+      const rect = canvas.getBoundingClientRect();
+      // console.log(event.pageX, event.clientX)
+      const x = event.x - rect.left;
+      const y = event.y - rect.top;
+      // console.log({x, y}, event)
 
-      const x = event.clientX;
-      const y = event.clientY;
-      
       lazy.update({ x, y });
-      lazyFriction.update({ x, y }, {friction: 0.5});
-      
-      const brush = lazy.getBrushCoordinates()
-      const brushFriction = lazyFriction.getBrushCoordinates()
+      lazyFriction.update({ x, y }, { friction: 0.5 });
 
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.drawImage(offscreenCanvas, 0, 0);
-        // Draw brush point
-        if(isDrawingRef.current){
-          offscreenCtx.beginPath()
-          offscreenCtx.fillStyle = 'blue'
-          offscreenCtx.arc(brushFriction.x, brushFriction.y, BRUSH_RADIUS, 0, Math.PI * 2, true)
-          offscreenCtx.lineTo(brushFriction.x, brushFriction.y);
-          offscreenCtx.fill()
+      //this make lazy the stroke
+      const brush = lazy.getBrushCoordinates();
+      // const brushFriction = lazyFriction.getBrushCoordinates();
+
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      ctx.drawImage(offscreenCanvas, 0, 0);
+
+      // Draw brush point IF
+      if (isDrawingRef.current) {
+        //this is for draw intermedial points in the stroke.
+
+        //here is when style of the draw most change.
+        points.push({ x, y });
+
+        let p1 = points[0];
+        let p2 = points[1];
+
+        if (p2 === undefined) return;
+
+        offscreenCtx.moveTo(p2.x, p2.y);
+        offscreenCtx.beginPath();
+
+        for (let i = 1, len = points.length; i < len; i++) {
+          // we pick the point between pi+1 & pi+2 as the
+          // end point and p1 as our control point
+          const midPoint = midPointBtw(p1, p2);
+          offscreenCtx.quadraticCurveTo(p1.x, p1.y, midPoint.x, midPoint.y);
+          p1 = points[i];
+          p2 = points[i + 1];
         }
-
-        ctx.beginPath()
-        ctx.fillStyle = 'blue'
-        ctx.arc(brushFriction.x, brushFriction.y, BRUSH_RADIUS, 0, Math.PI * 2, true)
-        ctx.fill()
-
-        //Draw the lazy radius.
-        ctx.beginPath()
-        ctx.strokeStyle = '#ccc'
-        ctx.arc(brush.x, brush.y, LAZY_RADIUS, 0, Math.PI * 2, true)
-        ctx.stroke()
+        //style the stoke
+        offscreenCtx.strokeStyle = "#000000";
+        offscreenCtx.lineWidth = 5;
+        offscreenCtx.lineCap = "round";
+        offscreenCtx.lineTo(p1.x, p1.y);
+        offscreenCtx.stroke();
       }
+      //stroke preview
+      ctx.beginPath();
+      ctx.fillStyle = "blue";
+      ctx.arc(x, y, BRUSH_RADIUS, 0, Math.PI * 2, true);
+      ctx.fill();
+
+      //Draw the lazy radius.
+      // ctx.beginPath();
+      // ctx.strokeStyle = "#ccc";
+      // ctx.arc(brush.x, brush.y, LAZY_RADIUS, 0, Math.PI * 2, true);
+      // ctx.stroke();
+    };
     const handleMouseUp = () => {
       isDrawingRef.current = false;
-      console.log("Up")
+      console.log("Up");
       offscreenCtx.closePath();
+      points.length = 0;
     };
 
-    canvas.addEventListener('mousedown', handleMouseDown);
-    canvas.addEventListener('mousemove', handleMouseMove);
-    window.addEventListener('mouseup', handleMouseUp);  // Capture mouse up globally
+    canvas.addEventListener("pointerdown", handleMouseDown);
+    canvas.addEventListener("pointermove", handleMouseMove);
+    window.addEventListener("pointerup", handleMouseUp); // Capture mouse up globally
+    // requestAnimationFrame(handleMouseMove);
 
+    return () => {
+      canvas.addEventListener("pointerdown", handleMouseDown);
+      canvas.removeEventListener("pointermove", handleMouseMove);
+      window.addEventListener("pointerup", handleMouseUp); // Capture mouse up globally
+    };
+  }, [lazy, lazyFriction]);
 
-
-  return () => {
-    canvas.addEventListener('mousedown', handleMouseDown);
-    canvas.removeEventListener('mousemove', handleMouseMove);
-    window.addEventListener('mouseup', handleMouseUp);  // Capture mouse up globally
-
-  };
-   
-  },[lazy, lazyFriction])
-
-
- 
   return (
     <>
-     <div className='bg-black w-full h-screen p-4'>
-        <canvas ref={canvasRef} className='bg-white w-full h-full rounded-lg'>
-        </canvas>
-    </div>
+      <div
+        className="bg-black w-full h-screen flex flex-col justify-center items-center
+      gap-4 p-4 box-border"
+      >
+        <div className="bg-white h-10 rounded-lg flex justify-center gap-1 items-center p-1">
+          <Hand
+            size={30}
+            className="hover:bg-zinc-200 rounded-md p-1 cursor-pointer transition-all active:bg-zinc-300 active:scale-[.9] text-zinc-700"
+          />
+          <Pencil
+            size={30}
+            className="hover:bg-zinc-200 rounded-md p-1 cursor-pointer transition-all active:bg-zinc-300 active:scale-[.9] text-zinc-700"
+          />
+          <PenLine
+            size={30}
+            className="hover:bg-zinc-200 rounded-md p-1 cursor-pointer transition-all active:bg-zinc-300 active:scale-[.9] text-zinc-700"
+          />
+          <Eraser
+            size={30}
+            className="hover:bg-zinc-200 rounded-md p-1 cursor-pointer transition-all active:bg-zinc-300 active:scale-[.9] text-zinc-700"
+          />
+        </div>
+        <div
+          ref={canvasContainerRef}
+          className="w-[700px] h-full hover:cursor-none"
+        >
+          <canvas
+            ref={canvasRef}
+            className="bg-white rounded-lg w-full h-full"
+          ></canvas>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/TODO.md
@@ -0,0 +1,3 @@
+## Issues
+-   no puedo hacer puntitos, tengo que si o si arrastrar el mouse para dibujar.
+-   no funciona del todo bien con la tableta.
